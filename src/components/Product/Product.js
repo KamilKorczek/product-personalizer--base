ca476d674c97ee84a969eba818bfd4058fbfc083
KamilKorczek <korczek@gmail.com>
2022-10-13 21:23:58 +0200

    The functionality of choosing the color of the t-shirt and changing the price of the t-shirt depending on the size selection has been added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,68 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import propTypes from 'prop-types';
+import { useState } from 'react';
 
 const Product = props => {
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
+  const getPrice = price => {
+    return price + props.sizes.find(({ name }) => name === currentSize).additionalPrice + '$'
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.clear();
+    console.log('Summary');
+    console.log('===============');
+    console.log('Name: ', props.title);
+    console.log('Price: ', getPrice(props.basePrice));
+    console.log('Size: ', currentSize);
+    console.log('Color: ', currentColor);
+  }
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={props.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>{getPrice(props.basePrice)}</span>
         </header>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map(size => 
+                <li key={size.name}>
+                  <button type="button" 
+                  className={clsx(size.name === currentSize && styles.active)} 
+                  onClick={() => setCurrentSize(size.name)}>{size.name}</button>
+                </li>)}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+            {props.colors.map(color =>
+              <li key={color}>
+                <button 
+                type="button" 
+                className={clsx(prepareColorClassName(color), color === currentColor && styles.active)} 
+                onClick={() => setCurrentColor(color)} />
+              </li>
+            )}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -41,6 +72,15 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes= {
+  basePrice: propTypes.number.isRequired,
+  colors: propTypes.array.isRequired,
+  id: propTypes.number.isRequired,
+  name: propTypes.string.isRequired,
+  sizes: propTypes.array.isRequired,
+  title: propTypes.string.isRequired
 };
 
 export default Product;
